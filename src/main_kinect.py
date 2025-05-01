a21d153dadd25a765e296d25582d4285568e829c
Nitin Miyyapuram <nitin.miyy@gmail.com>
2025-04-30 19:21:24 -0700

    backup

--- src/main_kinect.py
+++ src/main_kinect.py
@@ -83,105 +83,86 @@ def apply_postprocessing(mask):
     
     return mask
 
-def get():
-    
-    # idek
-    ret, thresh = cv.threshold(ir / ir_dsize, 1. - (1 / ir_dsize), 1.0, cv.THRESH_BINARY)
-    processed_mask = apply_postprocessing(thresh)
-    thresh_8bit = np.array(processed_mask, dtype=np.uint8)
-    visible_markers = cv.findContours(thresh_8bit, cv.RETR_LIST,
-        cv.CHAIN_APPROX_SIMPLE)
-    contours = imutils.grab_contours(visible_markers)
-    
-    # color_frame = cv.resize(color, (int(1920 / 3), int(1080 / 3)))
-    contours_poly = [None]*len(contours)
-    centers = np.zeros(shape=(len(contours), 2))
-    radius = np.zeros(shape=(len(contours)))
-    markers = np.zeros((thresh_8bit.shape[0], thresh_8bit.shape[1], 3), dtype=np.uint8)
-    ball = np.zeros((thresh_8bit.shape[0], thresh_8bit.shape[1], 3), dtype=np.uint8)
-
-    if len(contours) > 0:
-
-        # main program loop
-        
-        # get centers of markers and connect them to form full ball contour
-        # or go die and just use average positions of markers
-        for i, c in enumerate(contours):
-            contours_poly[i] = cv.approxPolyDP(c, 3, True)
-            centers[i], radius[i] = cv.minEnclosingCircle(contours_poly[i])
-            cv.circle(markers, (int(centers[i][0]), int(centers[i][1])), int(radius[i]), color=(0, 255, 0), thickness=2)
-        
-        # create shape known as "ball"
-        centers = np.array(centers, dtype=np.int32)
-        [x, y] = np.average(centers, axis=0)
-        avg_dist = np.average(np.array(list(map(lambda coords: 
-            np.sqrt((coords[0] - x) ** 2 + (coords[1] - y) ** 2), centers
-        ))))
-        cv.circle(ball, (int(x), int(y)), int(avg_dist), (0, 0, 255), 2)
-
 def sample():
-    for _ in range(10):
+
+    ball_coordinates = []
+
+    while len(ball_coordinates) < 10:
         frames = listener.waitForNewFrame()
-        ir = frames["ir"].asarray()
 
+        color_dsize = 2 ** 8
+        ir_dsize = 65535.
+        depth_dsize = 4500.
 
-while True:
-    frames = listener.waitForNewFrame()
+        color = frames["color"]
+        ir = frames["ir"]
+        depth = frames["depth"]
 
-    color_dsize = 2 ** 8
-    ir_dsize = 65535.
-    depth_dsize = 4500.
-    # color = frames["color"].asarray()
-    ir = frames["ir"].asarray()
-    # depth = frames["depth"].asarray()
-    
-    # registration.apply(color, depth, undistorted, registered,
-    #                    bigdepth=bigdepth,
-    #                    color_depth_map=color_depth_map)
-    
-    # depthimg = depth / 4500.
-    # colorimg = cv2.resize(color,
-    #                                (int(1920 / 3), int(1080 / 3)))
-    
-    ret, thresh = cv.threshold(ir / ir_dsize, 1. - (1 / ir_dsize), 1.0, cv.THRESH_BINARY)
-    processed_mask = apply_postprocessing(thresh)
-    thresh_8bit = np.array(processed_mask, dtype=np.uint8)
-    visible_markers = cv.findContours(thresh_8bit, cv.RETR_LIST,
-        cv.CHAIN_APPROX_SIMPLE)
-    contours = imutils.grab_contours(visible_markers)
-    
-    # color_frame = cv.resize(color, (int(1920 / 3), int(1080 / 3)))
-    contours_poly = [None]*len(contours)
-    centers = np.zeros(shape=(len(contours), 2))
-    radius = np.zeros(shape=(len(contours)))
-    markers = np.zeros((thresh_8bit.shape[0], thresh_8bit.shape[1], 3), dtype=np.uint8)
-    ball = np.zeros((thresh_8bit.shape[0], thresh_8bit.shape[1], 3), dtype=np.uint8)
+        registration.apply(color, depth, undistorted, registered,
+                           bigdepth=bigdepth,
+                           color_depth_map=color_depth_map)
 
-    if len(contours) > 0:
+        color = color.asarray()
+        ir = ir.asarray()
+        # depth = depth.asarray()
 
-        # main program loop
+        # depthimg = depth / 4500.
+        # colorimg = cv2.resize(color,
+        #                                (int(1920 / 3), int(1080 / 3)))
         
-        # get centers of markers and connect them to form full ball contour
-        # or go die and just use average positions of markers
-        for i, c in enumerate(contours):
-            contours_poly[i] = cv.approxPolyDP(c, 3, True)
-            centers[i], radius[i] = cv.minEnclosingCircle(contours_poly[i])
-            cv.circle(markers, (int(centers[i][0]), int(centers[i][1])), int(radius[i]), color=(0, 255, 0), thickness=2)
+        ret, thresh = cv.threshold(ir / ir_dsize, 1. - (1 / ir_dsize), 1.0, cv.THRESH_BINARY)
+        processed_mask = apply_postprocessing(thresh)
+        thresh_8bit = np.array(processed_mask, dtype=np.uint8)
+        visible_markers = cv.findContours(thresh_8bit, cv.RETR_LIST,
+            cv.CHAIN_APPROX_SIMPLE)
+        contours = imutils.grab_contours(visible_markers)
         
-        # create shape known as "ball"
-        centers = np.array(centers, dtype=np.int32)
-        [x, y] = np.average(centers, axis=0)
-        avg_dist = np.average(np.array(list(map(lambda coords: 
-            np.sqrt((coords[0] - x) ** 2 + (coords[1] - y) ** 2), centers
-        ))))
-        cv.circle(ball, (int(x), int(y)), int(avg_dist), (0, 0, 255), 2)
+        # color_frame = cv.resize(color, (int(1920 / 3), int(1080 / 3)))
+        contours_poly = [None]*len(contours)
+        centers = np.zeros(shape=(len(contours), 2))
+        radius = np.zeros(shape=(len(contours)))
+        markers = np.zeros((thresh_8bit.shape[0], thresh_8bit.shape[1], 3), dtype=np.uint8)
+        ball = np.zeros((thresh_8bit.shape[0], thresh_8bit.shape[1], 3), dtype=np.uint8)
+
+        if len(contours) > 0:
+
+            # main program loop
+            
+            # get centers of markers and connect them to form full ball contour
+            # or go die and just use average positions of markers
+            for i, c in enumerate(contours):
+                contours_poly[i] = cv.approxPolyDP(c, 3, True)
+                centers[i], radius[i] = cv.minEnclosingCircle(contours_poly[i])
+                cv.circle(markers, (int(centers[i][0]), int(centers[i][1])), int(radius[i]), color=(0, 255, 0), thickness=2)
+            
+            # create shape known as "ball"
+            centers = np.array(centers, dtype=np.int32)
+            [col, row] = np.average(centers, axis=0)
+            avg_dist = np.average(np.array(list(map(lambda coords: 
+                np.sqrt((coords[0] - col) ** 2 + (coords[1] - row) ** 2), centers
+            ))))
+            cv.circle(ball, (int(col), int(row)), int(avg_dist), (0, 0, 255), 2)
+
+            registration.undistortDepth(depth, undistorted)
+            x, y, z = registration.getPointXYZ(undistorted, row, col)
+            
+            ball_coordinates.append([depth.timestamp, x,y,z])
+
+        listener.release(frames)
+
+    return ball_coordinates
+
+
+
+if __name__ == "__main__":
+
+    while True:
         
         motion_study = sample()
-    # cv.imshow("thresholded img", processed_mask)
-    cv.imshow("markers", markers)
-    cv.imshow("ball", ball)
+        print(motion_study)
+        # cv.imshow("markers", markers)
+        # cv.imshow("ball", ball)
 
-    listener.release(frames)
-    key = cv.waitKey(delay=1)
-    if key == ord('q'):
-        break
+        key = cv.waitKey(delay=1)
+        if key == ord('q'):
+            break
